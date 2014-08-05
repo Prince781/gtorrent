9837537d1161b0144f5c0b4c22f53a71d90d27c2
Prince781 <princetonf@optonline.net>
2014-08-04 22:42:25 -0400

    Updating debug script.

--- scripts/gtorrent_debug.sh
+++ scripts/gtorrent_debug.sh
@@ -2,7 +2,7 @@
 
 function gtorrent_debug() {
 	local options="--tool=memcheck --leak-check=yes"
-	valgrind $options ./gTorrent $1 > valgrind_log.txt 2>&1
+	valgrind $options --log-file=valgrind.log ../gTorrent $1
 }
 
 gtorrent_debug $1
